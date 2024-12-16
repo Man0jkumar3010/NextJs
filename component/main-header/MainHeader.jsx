2e65894414117c57e95ac03222a8fd6937a60432
Man0jkumar3010 <manojkumar.fsd3@gmail.com>
2024-12-16 15:21:00 +0530

    Created next js setup

---
+++ component/main-header/MainHeader.jsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./MainHeaderBackground";
+import { usePathname } from "next/navigation";
+import NavLink from "./NavLink";
+
+export default function MainHeader() {
+  const path = usePathname();
+  return (
+    <>
+      <MainHeaderBackground />
+      <header id="main-header" className={classes.header}>
+        <div id="logo">
+          <Link href="/" className={classes.logo}>
+            <Image src={logoImg} alt="logo" priority />
+            NextLevel Food
+          </Link>
+        </div>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href='/meals'>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href='/community'>Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
